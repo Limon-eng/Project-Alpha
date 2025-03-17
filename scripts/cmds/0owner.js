c63f0aa635a8c6d8623436d1022ccfcf6fb50fdf
Limon-eng <limonbapari6@gmail.com>
2025-03-18 03:30:00 +0600

    owner.js

--- scripts/cmds/0owner.js
+++ scripts/cmds/0owner.js
@@ -1,64 +1,71 @@
+const { GoatWrapper } = require('fca-liane-utils');
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 
 module.exports = {
-config: {
-  name: "owner",
-  aurthor:"Tokodori",// Convert By Goatbot Tokodori 
-   role: 0,
-  shortDescription: " ",
-  longDescription: "",
-  category: "admin",
-  guide: "{pn}"
-},
+	config: {
+		name: "owner",
+		author: "Limon",
+		role: 0,
+		shortDescription: " ",
+		longDescription: "",
+		category: "admin",
+		guide: "{pn}"
+	},
 
-  onStart: async function ({ api, event }) {
-  try {
-    const ownerInfo = {
-      name: 'Limon Hossain Shuvo',
-      gender: 'Male',
-      age: '13-10-2007',
-      height: '5'10"',
-      facebookLink: 'https://www.facebook.com/adminn.2018',
-      nick: 'Shuvo'
-    };
+	onStart: async function ({ api, event }) {
+		try {
+			const ownerInfo = {
+				name: 'Limon Hossain Shuvo',
+				gender: '𝑴𝒂𝑳𝒆',
+				Birthday: '13-10-𝟐𝟎𝟎7',
+				religion: '𝙄𝒔𝒍𝑨𝒎',
+				hobby: '𝑺𝒍𝒆𝒆𝑷𝒊𝒏𝑮',
+				Fb: 'https://www.facebook.com/adminn.2018',
+				Relationship: '𝑺𝒊𝒏𝑮𝒆𝒍',
+				Height: '5"10'
+			};
 
-    const bold = 'https://i.imgur.com/fGY8plC.mp4'; // Replace with your Google Drive videoid link https://drive.google.com/uc?export=download&id=here put your video id
+			const bold = 'https://imgur.com/8sgLnC6.mp4';
+			const tmpFolderPath = path.join(__dirname, 'tmp');
 
-    const tmpFolderPath = path.join(__dirname, 'tmp');
+			if (!fs.existsSync(tmpFolderPath)) {
+				fs.mkdirSync(tmpFolderPath);
+			}
 
-    if (!fs.existsSync(tmpFolderPath)) {
-      fs.mkdirSync(tmpFolderPath);
-    }
+			const videoResponse = await axios.get(bold, { responseType: 'arraybuffer' });
+			const videoPath = path.join(tmpFolderPath, 'owner_video.mp4');
 
-    const videoResponse = await axios.get(bold, { responseType: 'arraybuffer' });
-    const videoPath = path.join(tmpFolderPath, 'owner_video.mp4');
+			fs.writeFileSync(videoPath, Buffer.from(videoResponse.data, 'binary'));
 
-    fs.writeFileSync(videoPath, Buffer.from(videoResponse.data, 'binary'));
+			const response = `
+◈ 𝖮𝖶𝖭𝖤𝖱 𝖨𝖭𝖥𝖮𝖱𝖬𝖠𝖳𝖨𝖮𝖭:\n
+ ~Name: ${ownerInfo.name}
+ ~Gender: ${ownerInfo.gender}
+ ~Birthday: ${ownerInfo.Birthday}
+ ~Religion: ${ownerInfo.religion}
+ ~Relationship: ${ownerInfo.Relationship}
+ ~Hobby: ${ownerInfo.hobby}
+ ~Fb: ${ownerInfo.Fb}
+ ~Height: ${ownerInfo.Height}
+			`;
 
-    const response = `
-Owner Information:🧾
-Name: ${ownerInfo.name}
-Gender: ${ownerInfo.gender}
-Age: ${ownerInfo.age}
-Height: ${ownerInfo.height}
-Facebook: ${ownerInfo.facebookLink}
-Nick: ${ownerInfo.nick}
-`;
+			await api.sendMessage({
+				body: response,
+				attachment: fs.createReadStream(videoPath)
+			}, event.threadID, event.messageID);
 
+			fs.unlinkSync(videoPath);
 
-    await api.sendMessage({
-      body: response,
-      attachment: fs.createReadStream(videoPath)
-    }, event.threadID, event.messageID);
-
-    if (event.body.toLowerCase().includes('ownerinfo')) {
-      api.setMessageReaction('🚀', event.messageID, (err) => {}, true);
-    }
-  } catch (error) {
-    console.error('Error in ownerinfo command:', error);
-    return api.sendMessage('An error occurred while processing the command.', event.threadID);
-  }
-},
+			api.setMessageReaction('😍', event.messageID, (err) => {}, true);
+		} catch (error) {
+			console.error('Error in ownerinfo command:', error);
+			return api.sendMessage('An error occurred while processing the command.', event.threadID);
+		}
+	}
 };
+
+const wrapper = new GoatWrapper(module.exports);
+wrapper.applyNoPrefix({ allowPrefix: true });
+      
